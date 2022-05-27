a131d8f15964295221bfc3f624da2d6bded8bcee
dolinkue <dolinkue_n@me.com>
2022-05-27 16:45:50 -0300

    add new item with alert

--- Todo/TodoListViewController.swift
+++ Todo/TodoListViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     
-    let itemArray = ["milk","chocolate", "huevos" ]
+    var itemArray = ["milk","chocolate", "huevos" ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    
+    // MARK: TableView Datasource Methods
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -31,7 +35,46 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
+    // MARK: Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+ 
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    // MARK: add new Item
 
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        
+        var texField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new items", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            self.itemArray.append(texField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { alertText in
+            alertText.placeholder = "Create new item"
+            texField = alertText
+        }
+        
+        alert.addAction(action)
+            
+        present(alert, animated: true, completion: nil)
+        
+    }
     
 }
 
